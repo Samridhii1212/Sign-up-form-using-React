a88c87ed142cfed748151f48853281bfa6252d1b
Samridhii1212 <115480641+Samridhii1212@users.noreply.github.com>
2023-04-29 13:08:08 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import reactdom from "react-dom";
+import Signup from "./form";
+
+
+
+reactdom.render(
+    <>
+    <Signup/>
+    </>,
+    document.getElementById("root")
+);
